9afeafbfd4c9a35d6a38a0d4752424aca362db17
EviBera <beraevi@gmail.com>
2024-06-19 08:54:44 +0200

    Add routing

--- MyPorfolioPage/src/app/app-routing.module.ts
+++ MyPorfolioPage/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent, title: 'Évi Bera - About'},
+  { path: 'projects', component: ProjectsComponent, title: 'Évi Bera - Projects'},
+  { path: 'contact', component: ContactComponent, title: 'Évi Bera - Contact'},
+  { path: '', redirectTo: '/about', pathMatch: 'full'},
+  { path: '**', redirectTo: '/about', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
