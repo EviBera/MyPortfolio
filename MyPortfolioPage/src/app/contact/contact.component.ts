04cffd2ce523aa31ccf60a546dfb0171eb49605c
EviBera <beraevi@gmail.com>
2024-07-08 16:57:30 +0200

    Add contact form and message sending logic

--- MyPortfolioPage/src/app/contact/contact.component.ts
+++ MyPortfolioPage/src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'mpp-contact',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  form: FormGroup = this.formBuilder.group({
+    from_name: '',
+    from_email: '',
+    message: ''
+  });
+
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  async send(){
+    emailjs.init('VHI-vVdX1-72cIw2p');
+    let response = await emailjs.send("service_xdmy7zr","template_opq3tnj",{
+      from_name: this.form.value.from_name,
+      from_email: this.form.value.from_email,
+      message: this.form.value.message,
+    });
+
+    alert('Your message has been sent.');
+
+    this.form.reset();
+  }
 }
+
+/*
+Code for using EmailJS:
+  emailjs.send("service_xdmy7zr","template_opq3tnj",{
+    from_name: "Test User",
+    from_email: "testuser@email.com",
+    message: "Nice.",
+  });
+*/
